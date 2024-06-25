1e44253ec88ea82f754ef137ac30bf99e07557bc
Manohar192372246 <160740765+Manohar192372246@users.noreply.github.com>
2024-06-25 12:23:02 +0530

    Add files via upload

---
+++ greatest three numbers.py
@@ -0,0 +1,11 @@
+a = eval(input("Enter num1: "))
+b = eval(input("Enter num2: "))
+c = eval(input("Enter num3: "))
+
+if a > b and a > c:
+    print("a is greater")
+elif b > a and b > c:
+    print("b is greater")
+else:
+    print("c is greater")
+
